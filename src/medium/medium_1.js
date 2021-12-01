21afb3f0f53046353c8f3345a4e1dd1ba71c63b0
aryamana193 <69406564+aryamana193@users.noreply.github.com>
2021-11-30 20:44:34 -0500

    f

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -8,7 +8,11 @@ import {variance} from "./data/stats_helpers.js";
  * prototype functions. Very useful
  */
 export function getSum(array) {
-
+    let solution = 0;
+    for(let i =0;i<array.length; i++){
+        solution = solution +solution[i];
+    }
+    return solution;
 }
 
 
