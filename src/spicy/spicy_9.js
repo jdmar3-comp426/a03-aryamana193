434dec6c0b168df73ec0fbcc76cdca83ce214ce7
aryamana193 <69406564+aryamana193@users.noreply.github.com>
2021-11-30 21:36:06 -0500

    pls

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -174,14 +174,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-    const test = (num)=>{
-        if(num%2==1){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    everyEven(arr, test)
+    everyEven(arr, x=> x%2 === 1)
 };
 
 
@@ -191,7 +184,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    someEven(arr, x=> x%2 === 1)
 };
 
 
@@ -202,5 +195,11 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
+    let solution = filter(arr, test);
+    if(solution.pass.length==n){
+        return true;
+    }else{
+        return false;
+    }
 
 };
