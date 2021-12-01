b42151c1d0a09b2a02b675d23e441fe6984bb532
aryamana193 <69406564+aryamana193@users.noreply.github.com>
2021-11-30 20:19:25 -0500

    tired

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -43,8 +43,8 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    min = numbers[0];
-    max = numbers[0];
+    let min = numbers[0];
+    let max = numbers[0];
     for (let i = 1; i <= numbers.length; i++) {
         if(numbers[i]>max){max = numbers[i]}
         if(numbers[i]<min){min = numbers[i]}
