c809a2ee8d9fd0da277a450c2383a28a49ddbd73
aryamana193 <69406564+aryamana193@users.noreply.github.com>
2021-11-30 20:51:25 -0500

    h

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,7 +26,15 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-
+    array.sort((a,b) => a-b);
+    let solution;
+    if(array.length % 2 == 1){
+        return array[Math.floor(len/2)];
+    }
+    else{
+        solution = (array[len/2] + array[(len/2)-1])/2;
+        return solution;
+    }
 }
 
 /**
